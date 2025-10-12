47edd510dbc81986394d640a9c3403d727ee1711
DuyetBKU <khongcostenn@gmail.com>
2025-10-12 22:49:05 +0700

    Update FDM-1D-Wave.m

--- Lesson5/FDM-1D-Wave.m
+++ Lesson5/FDM-1D-Wave.m
@@ -13,7 +13,6 @@ u(:, 2) = u(:, 1);
 alpha = (c * dt / dx)^2;   
 for n = 2:M
    for i = 2:N
-       % Finite difference
        u(i, n+1) = 2 * u(i, n) - u(i, n-1) + ...
                    alpha * (u(i+1, n) - 2 * u(i, n) + u(i-1, n));
    end
